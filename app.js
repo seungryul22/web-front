590c0ba631d5cc2addb2bbd3da5e40d8905ac481
seungtyul22 <seunglul1999@gmail.com>
2022-05-28 21:56:37 +0900

    5.28
    자바스크립트를 이용한 DOM 기능 추가하는중

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 function searchBar (){
-    document.write("")
-}
+
+   var search = document.querySelector("#search_bar");
+   var value = search.getAttribute("value");
+    // 함수 기능 
+    if(value == "on"){
+        search.innerHTML="<div class='x'><input type='text' placeholder='여기에 입력하세요'><input type='button' value='닫기'</div>";       
+
+    }
+};
